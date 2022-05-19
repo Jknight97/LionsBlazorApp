b6732117a1e01e3f6584ecc2014a3470c3495a19
Joshua Knight <83534575+Jknight97@users.noreply.github.com>
2022-05-19 19:09:50 -0400

    lotta changes

---
+++ LionsApp/Data/AdminData.cs
@@ -0,0 +1,14 @@
+namespace LionsApp.Data;
+
+public class AdminData {
+
+    //Should I pull data in order from a database or create objects of each admin?
+    //Objects would take more RAM
+    //Database takes more time but probably better. Could tag each individual with their department and then pull names/titles in alphabetical order by department when called at each department listing    
+
+    //Could create objects of each department and then display those
+    //But where else would I use those objects? No where else really
+    //yeah objects aren't helpful here
+    //Make a database of staff, tag them by department, then populate department tables alphabetically
+    //
+}
